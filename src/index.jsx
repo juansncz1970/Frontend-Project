0cc9a30f0a3d9e7c042e79dbe766c3a957d27759
juansncz1970 <jfvolante@gbox.ncf.edu.ph>
2024-11-13 23:11:15 +0800

    modified:   src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  // Use this for React 18+
 import App from './App';  // Import the root App component
-import './index.css';  // Import the global CSS
+//import './index.css';  // Import the global CSS
 
 // This is for React 18 and later. ReactDOM.createRoot is the new way of rendering.
 const root = ReactDOM.createRoot(document.getElementById('root'));
