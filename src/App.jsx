c84337741c4a4128d15ee9477b7239f9a4227d42
juansncz1970 <jfvolante@gbox.ncf.edu.ph>
2024-11-13 21:21:23 +0800

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Education from './pages/Education';
 import Hobbies from './pages/Hobbies';
 import Contact from './pages/Contact';
+import './App.css';  // Optional: Include if you have custom styles
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/hobbies" element={<Hobbies />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="app">
+        <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/education">Education</Link>
+          <Link to="/hobbies">Hobbies</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/hobbies" element={<Hobbies />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
+
+export default App;
