d60ee48cf01031c2d75700133fd8736e1f923945
juansncz1970 <jfvolante@gbox.ncf.edu.ph>
2024-11-18 18:12:28 +0800

    add new filder styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './app.css';
+import './styles/app.css';
 
 import Home from './pages/Home';
 import About from './pages/About';
